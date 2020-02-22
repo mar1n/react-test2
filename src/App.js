26295f808850b20bd0c7a5385970c58da48c5ff3
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-22 23:32:23 +0000

    add body file for app.js simple counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const doIncrement = (prevState) => ({
+  counter: prevState.counter + 1,
+});
+const doDecrement = (prevState) => ({
+  counter: prevState.counter - 1,
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        <h1>My Counter</h1>
+        <p>{counter}</p>
+        <button
+          type="button"
+          onClick={this.onIncrement}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Increment
+        </button>
+        <button
+          type="button"
+          onClick={this.onDecrement}
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
